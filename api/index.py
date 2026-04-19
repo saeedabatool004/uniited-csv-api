7c57f0f74e1bdff10ba3bddea299e8586ea80c85
Saeeda Batool <saeeda batool>
2026-04-19 14:41:42 +0100

    restructure for vercel

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/api/data", methods=["GET"])
 def get_data():
     rows = []
-    base_dir = os.path.dirname(os.path.abspath(__file__))
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     csv_path = os.path.join(base_dir, "data.csv")
     with open(csv_path, newline="", encoding="utf-8") as f:
         reader = csv.DictReader(f)
